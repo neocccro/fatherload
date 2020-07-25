baca43f97640880a3b5f32fe17c776ec70a51ea7
coen <cavandiepen@gmail.com>
2020-07-25 06:00:36 +0200

    started on a loading bar

--- Assets/ColorWheel.cs
+++ Assets/ColorWheel.cs
@@ -2,7 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorWheel : MonoBehaviour
+public static class ColorWheel
 {
+    public static Color GetColor(float input)
+    {
+        float r, g, b;
+        r = g = b = 0;
 
+        input *= 6;
+
+        if (input < 1)
+        {
+            r = 0;
+            g = 1;
+            b = input;
+        }
+        else if (input < 2)
+        {
+            input -= 1;
+            r = 0;
+            g = 1 - input;
+            b = 1;
+        }
+        else if (input < 3)
+        {
+            input -= 2;
+            r = input;
+            g = 0;
+            b = 1;
+        }
+        else if (input < 4)
+        {
+            input -= 3;
+            r = 1;
+            g = 0;
+            b = 1 -input;
+        }
+        else if (input < 5)
+        {
+            input -= 4;
+            r = 1;
+            g = input;
+            b = 0;
+        }
+        else
+        {
+            input -= 5;
+            r = 1 - input;
+            g = 1;
+            b = 0;
+        }
+        return new Color(r, g, b);
+    }
 }
---
+++ Assets/LoadingBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Slider slider;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.color = ColorWheel.GetColor(0.7f - slider.value*0.65f);
+    }
+
+
+}
